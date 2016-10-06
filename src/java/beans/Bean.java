8bd5b500bf03f4287c4b5b399dce909e846451c3
am <am@W0268>
2016-10-06 09:54:23 +0900

    テスト

---
+++ src/java/beans/Bean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author am
+ */
+@Named(value = "bean")
+@Dependent
+public class Bean {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean() {
+    }
+    
+}
